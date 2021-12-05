b16d159cf57f81aa4453a53c7c36c07bd67c0f3d
tj_ir <tj_ir@DESKTOP-G74I39B.lan>
2021-12-05 13:01:02 -0600

    Created initialize function. Fills ArrayList<Survivor> and ArrayList<Zombie> objects with random Object subclasses of the appropriate type. Created global variable for Max Number of Characters; what should the size limit be?

--- ZombieWar/src/ZombieWar.java
+++ ZombieWar/src/ZombieWar.java
@@ -1,26 +1,25 @@
+
+import java.util.ArrayList;
+
+
 // Main class 
 
 public class ZombieWar {
-
+    
+    private static final int MAX_CHARACTERS = 20;
+    private static final ArrayList<Survivor> survivors = new ArrayList<>();
+    private static final ArrayList<Zombie> zombies = new ArrayList<>();
+    
     public static void main(String[] args) {
-        Survivor survivor1 = new Soldier();
-        System.out.println("Survivor class:  " + survivor1.getName());
-        System.out.println("Survivor attack: " + survivor1.getAttack());
-        System.out.println("Survivor health: " + survivor1.getHealth() + "\n");
         
-        Zombie zombie1 = new Tank();
-        System.out.println("Zombie class:  " + zombie1.getName());
-        System.out.println("Zombie attack: " + zombie1.getAttack());
-        System.out.println("Zombie health: " + zombie1.getHealth() + "\n");
-        
-        survivor1.attack(zombie1);
-        System.out.println("Zombie class:  " + zombie1.getName());
-        System.out.println("Zombie health: " + zombie1.getHealth() + "\n");
-        
+        int survivorCount = (int)(1 + Math.random() * MAX_CHARACTERS);
+        int zombieCount = (int)(1 + Math.random() * MAX_CHARACTERS);
+        initialize(survivorCount, zombieCount);
+
         int initialSurvivorNum = 17;
         int afterMathSurvivorNum = 6;
         int zombieNum = 8;
-
+        
         SampleRun1(initialSurvivorNum, afterMathSurvivorNum, zombieNum);
 
     }
@@ -32,5 +31,64 @@ public class ZombieWar {
                 + " have made it to safety.";
         System.out.print(outcome);
     }
-
+    
+    
+    /**
+     * Fills ArrayLists survivors and zombies. Subclasses for each chosen randomly.
+     * 
+     * @param survivorCount - Integer for number of Survivors to create
+     * @param zombieCount   - Integer for number of Zombies to create
+     * 
+     * Author: Tyler Irvin
+     * Revisions (work, person):
+     */
+    private static void initialize(int survivorCount, int zombieCount)
+    {
+        for (int i = 0; i < survivorCount; i++)
+        {
+            int randomClass = (int)(Math.random() * 3);
+            switch (randomClass)
+            {
+                case 0  -> 
+                    {survivors.add(new Child());}
+                case 1  ->
+                    {survivors.add(new Teacher());}
+                default ->
+                    {survivors.add(new Soldier());}
+            }
+        }
+        
+        for (int i = 0; i < zombieCount; i++)
+        {
+            int randomClass = (int)(Math.random() * 2);
+            switch (randomClass)
+            {
+                case 0  ->
+                    {zombies.add(new CommonInfected());}
+                default ->
+                    {zombies.add(new Tank());}
+            }
+        }
+        
+        System.out.println("We have " + survivors.size() + " survivors trying to make it to safety.");
+        System.out.println("But there are " + zombies.size() + " zombies waiting for them.");
+        
+/*          // TESTING output
+        System.out.println("\nSURVIVORS:\n");
+        for (int i = 0; i < survivors.size(); i++)
+        {
+            System.out.println("Survivor class:  " + survivors.get(i).getName());
+            System.out.println("Survivor attack: " + survivors.get(i).getAttack());
+            System.out.println("Survivor health: " + survivors.get(i).getHealth() + "\n");
+        }
+        
+        System.out.println("\nZOMBIES:\n");
+        for (int i = 0; i < zombies.size(); i++)
+        {
+            System.out.println("Zombie class:  " + zombies.get(i).getName());
+            System.out.println("Zombie attack: " + zombies.get(i).getAttack());
+            System.out.println("Zombie health: " + zombies.get(i).getHealth() + "\n");            
+        }
+*/
+    }
 }
