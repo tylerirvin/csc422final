feb77daa758ba103f2cc2275045bea6f626ff4f3
tj_ir <tj_ir@DESKTOP-G74I39B.lan>
2021-12-05 23:22:52 -0600

    Adjust attack method to not allow if either object isDead(). Added printout.

---
+++ ZombieWar/src/Zombie.java
@@ -0,0 +1,61 @@
+public class Zombie {
+
+    private int health;
+    private int attack;
+    private String name;
+
+    protected Zombie(int health, int attack, String name){
+        this.health = health;
+        this.attack = attack;
+        this.name = name;
+    }
+
+    
+    public int getAttack() {
+        return attack;
+    }
+
+    
+    public int getHealth() {
+        return health;
+    }
+    
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    
+    public boolean isDead()
+    {
+        return health <= 0;
+    }
+    
+    
+    public void attack(Survivor survivor)
+    {
+        if (!survivor.isDead() && !this.isDead())
+        {
+            survivor.setHealth(survivor.getHealth() - this.attack);
+            if(survivor.isDead())
+                System.out.println(this.name + " killed " + survivor.getName() + ".");
+        }
+    }
+}
